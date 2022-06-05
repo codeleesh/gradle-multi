c86443d7348a673f6638eed89b498a34bb4083ca
lovethefeel <lovethefeel@gmail.com>
2022-06-05 21:32:27 +0900

    init multi-project

---
+++ app-common/src/main/java/me/lovethefeel/common/Const.java
@@ -0,0 +1,10 @@
+package me.lovethefeel.common;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class Const {
+    public static final String STRING_EMPTY = "";
+}
